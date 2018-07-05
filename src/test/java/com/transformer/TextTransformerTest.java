445c2f2b2391737bc29be9b1fa4358779e49eb05
rjerome <rjerome@architech.ca>
2018-07-05 00:37:43 -0400

    Add more unit tests

--- src/test/java/com/transformer/TextTransformerTest.java
+++ src/test/java/com/transformer/TextTransformerTest.java
@@ -10,31 +10,80 @@ public class TextTransformerTest {
     private TextTransformer transformer;
 
     @Test
-    public void textTransformerInitializationUnifiesCase() {
+    public void initializationConvertsInputToLowerCase() {
         transformer = new TextTransformer("lOrEM", "H,V,1");
-        assertArrayEquals(transformer.getInputChars(), new String[] { "l", "o", "r", "e", "m" });
-        assertArrayEquals(transformer.getTransformations(), new String[] { "H", "V", "1" });
+        assertArrayEquals(new String[] { "l", "o", "r", "e", "m" }, transformer.getInputChars());
+        assertArrayEquals(new String[] { "H", "V", "1" }, transformer.getTransformations());
     }
 
     @Test
-    public void textTransformerInitializationRemovesEmptySpaceInTransformations() {
+    public void initializationIgnoresWhiteSpaceInTransformations() {
         transformer = new TextTransformer("lOrEM", "H,     V, 1");
-        assertArrayEquals(transformer.getInputChars(), new String[] { "l", "o", "r", "e", "m" });
-        assertArrayEquals(transformer.getTransformations(), new String[] { "H", "V", "1" });
+        assertArrayEquals(new String[] { "l", "o", "r", "e", "m" }, transformer.getInputChars());
+        assertArrayEquals(new String[] { "H", "V", "1" }, transformer.getTransformations());
     }
 
     @Test
-    public void textTransformerTransformsTextValue() {
+    public void singleHorizontalFlipTransformation() {
+        transformer = new TextTransformer("lOrEM", "H");
+        String transformed = transformer.transform();
+        assertEquals("swuiv", transformed);
+    }
+
+    @Test
+    public void singleVerticalFlipTransformation() {
+        transformer = new TextTransformer("lOrEM", "V");
+        String transformed = transformer.transform();
+        assertEquals("olfd7", transformed);
+    }
+
+    @Test
+    public void singleLinearShiftWithPositiveOffset() {
+        transformer = new TextTransformer("lOrEM", "1");
+        String transformed = transformer.transform();
+        assertEquals(";ptr,", transformed);
+    }
+
+    @Test
+    public void singleLinearShiftWithNegativeOffset() {
+        transformer = new TextTransformer("lOrEM", "-1");
+        String transformed = transformer.transform();
+        assertEquals("kiewn", transformed);
+    }
+
+    @Test
+    public void singleLinearShiftWithZeroOffset() {
+        transformer = new TextTransformer("lOrEM", "0");
+        String transformed = transformer.transform();
+        assertEquals("lorem", transformed);
+    }
+
+    @Test
+    public void singleLinearShiftWithEffectiveZeroOffset() {
+        transformer = new TextTransformer("lOrEM", "40");
+        String transformed = transformer.transform();
+        assertEquals("lorem", transformed);
+    }
+
+    @Test
+    public void horizontalFlipFollowedByHorizontalFlipCausesNoTransformation() {
+        transformer = new TextTransformer("lOrEM", "H,H");
+        String transformed = transformer.transform();
+        assertEquals("lorem", transformed);
+    }
+
+    @Test
+    public void verticalFlipFollowedByVerticalFlipCausesNoTransformation() {
+        transformer = new TextTransformer("lOrEM", "V,V");
+        String transformed = transformer.transform();
+        assertEquals("lorem", transformed);
+    }
+
+    @Test
+    public void horizontalVerticalAndLinearTransformations() {
         transformer = new TextTransformer("lOrEM", "H,V,1");
         String transformed = transformer.transform();
-        assertEquals(transformed, "edkl5");
-    }
-
-    @Test
-    public void textTransformerTransformsSameTransformationMultipleTimes() {
-        transformer = new TextTransformer("lOrEM", "H,H,1");
-        String transformed = transformer.transform();
-        assertEquals(transformed, ";ptr,");
+        assertEquals("edkl5", transformed);
     }
 
 }
